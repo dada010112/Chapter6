366995ef130e99ba892567c3cd7e2fab7b9f8d19
dahye4368 <dahye4368@daum.net>
2023-03-31 16:45:49 +0900

    초기 커밋.

---
+++ YangDaHye_ex.02/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace YangDaHye_ex._02
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, ref mean);
+
+            Console.WriteLine($"평균 : {mean}");
+        }
+
+        public static void Mean(double a, double b, double c, double d, double e, ref double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
